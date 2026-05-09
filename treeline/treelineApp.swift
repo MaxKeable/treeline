e573c87a70e798b91265db9ce0993d98409bf1d0
Max Keable <max.keable488@gmail.com>
2026-05-09 11:27:03 +1000

    Initial Commit

---
+++ treeline/treelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  treelineApp.swift
+//  treeline
+//
+//  Created by Max Keable  on 9/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct treelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
